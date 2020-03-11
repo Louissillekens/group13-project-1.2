e2cb648e0309a403888d25c087cde101fe0354db
Clément Detry <43852207+clem2507@users.noreply.github.com>
2020-03-11 12:46:15 +0100

    delete

--- FirstPhaseCode/src/PuttingSimulator.java
+++
@@ -1,7 +0,0 @@
-public class PuttingSimulator {
-
-    public PuttingSimulator(PuttingCourse, PhysicsEngine);
-    public void set_ball_position(Vector2d);
-    public void get_ball_position();
-    public void take_shot(Vector2d initial_ball_velocity);
-}

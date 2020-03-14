abc671d5d8238fd75258e863d77d19043d15d772
clemdetry <c.detry@student.maastrichtuniversity.nl>
2020-03-14 16:46:08 +0100

    updates on Function2d and PuttingCourse

--- src/src/Board/Vector2d.java
+++ src/src/Board/Vector2d.java
@@ -1,22 +1,15 @@
+
 package Board;
 
 public class Vector2d {
 
     private double x;
     private double y;
-    private double mu;
-    private double g;
-    private double vmax;
-    private double tolerance;
 
-    public Vector2d(double x, double y, double mu, double g, double vmax, double tolerance) {
+    public Vector2d(double x, double y) {
 
         this.x = x;
         this.y = y;
-        this.mu = mu;
-        this.g = g;
-        this.vmax = vmax;
-        this.tolerance = tolerance;
     }
 
     // Get the x position of a vector
@@ -29,29 +22,5 @@ public class Vector2d {
     public double get_y() {
 
         return y;
-    }
-
-    // Get the coefficient of friction of a vector
-    public double get_mu() {
-
-        return mu;
-    }
-
-    // Get the gravity of a vector
-    public double get_g() {
-
-        return g;
-    }
-
-    // Get the max velocity of a vector
-    public double get_vmax() {
-
-        return vmax;
-    }
-
-    // Get the tolerance distance of a vector
-    public double get_tolerance() {
-
-        return tolerance;
     }
 }

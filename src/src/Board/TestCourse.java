3dc5a15f9146e377202025a3447ccae8a6cfdbce
Louissillekens <56159154+Louissillekens@users.noreply.github.com>
2020-03-15 13:13:42 +0000

    made an easy flat testcourse

--- src/src/Board/TestCourse.java
+++ src/src/Board/TestCourse.java
@@ -2,14 +2,18 @@ package Board;
 
 public class TestCourse {
 
-    double[][] board = {{1   , 1.2 , 1.3 , 1.3 , 1.1 , 1   , 0   ,-1  },
+    /*double[][] board = {{1   , 1.2 , 1.3 , 1.3 , 1.1 , 1   , 0   ,-1  },
                         {1.05, 1.15, 1.2 , 1.2 , 1.1 , 0.9 , 0.55, 0  },
                         {1.1 , 1.2 , 1.2 , 1.25, 1.34, 1   , 0.33, 0.1},
-                        {1.43, 1.23, 1.18, 0.9 , 0.4 , 0   ,-0.4 ,-1.2}};
+                        {1.43, 1.23, 1.18, 0.9 , 0.4 , 0   ,-0.4 ,-1.2}};*/
+
+    double[][] board = new double[10][10];//a double array of 0's which gives a flat surface on which we can test stuff
+
+
     double out_of_bounds_height = 1;//outside of the array the height is just 1
 
     Vector2d start = new Vector2d(0,0);
-    Vector2d hole = new Vector2d(3,3);
+    Vector2d hole = new Vector2d(7,6);
 
     final double gravity = 9.81;
     final double max_velocity = 3;

23f4725186d5e35d609a701da2da2c397136e56d
Louissillekens <56159154+Louissillekens@users.noreply.github.com>
2020-03-17 19:13:44 +0000

    Update MakeTestCourse.java

--- project2/core/src/com/project2/test/MakeTestCourse.java
+++ project2/core/src/com/project2/test/MakeTestCourse.java
@@ -3,24 +3,33 @@ package com.project2.test;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g3d.Environment;
-import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
-import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.math.Vector3;
 
+
 public class MakeTestCourse extends ApplicationAdapter implements InputProcessor {
 
+    private PerspectiveCamera cam;
+    private ModelBatch modelbatch;
+    private ModelBuilder modelbuilder;
+    private Model box;
+    private ModelInstance modelInstance;
+    private Environment environment;
 
     @Override
     public void create () {
 
+        cam = new PerspectiveCamera(75, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(0f, 0f, 5f);
+        Vector3 origin = new Vector3(0f,0f,0f);
+        cam.lookAt(origin);
+        cam.near = 0.1f;
+        cam.far = 250f;
     }
 
     @Override

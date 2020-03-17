00a4911ef0f282286cfb5c94c3cdee0937070068
Alexandre Martens <56159200+AlexMartens12@users.noreply.github.com>
2020-03-17 13:22:06 +0100

    libGDX test in project2 folder

---
+++ project2/desktop/src/com/project2/test/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.project2.test.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.project2.test.GdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxGame(), config);
+	}
+}

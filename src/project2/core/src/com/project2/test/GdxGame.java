82df31e530a8b2fa11a27cb717d752025ea2edd1
ReinKengen <kengenrein@gmail.com>
2020-03-18 11:38:54 +0100

    Update GdxGame.java

--- src/project2/core/src/com/project2/test/GdxGame.java
+++ src/project2/core/src/com/project2/test/GdxGame.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GdxGame extends ApplicationAdapter {
-	//small change
 	SpriteBatch batch;
 	Texture img;
 	
